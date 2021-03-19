6868ae325acd8460b7580c361b463ee23544bacf
EliottBICS <eliottbonte@gmail.com>
2021-03-19 14:55:10 +0100

    Sign In screen implemented - Now moving to the sign up screen
    (The sign in screen is made so that dimensions of the elements can be easily tweaked)

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -19,12 +19,15 @@ class _SignInState extends State<SignIn> {
         elevation: 1,
       ),
       body: Form(
+        key : _formKey,
         child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10),
           child: Column(
             children: [
               Spacer(),
               TextFormField(
                 validator: (val) {
+                  print("empty mail");
                   return val.isEmpty ? "The mail field is empty" : null;
                 },
                 decoration: InputDecoration(hintText: "email"),
@@ -34,13 +37,45 @@ class _SignInState extends State<SignIn> {
               ),
               TextFormField(
                 validator: (val) {
-                  return val.isEmpty ? "The password field is empty" : null;
+                  if (val.isEmpty) {
+                    return "The password field is empty";
+                  } else {
+                    return null;
+                  }
                 },
                 decoration: InputDecoration(hintText: "password"),
                 onChanged: (val) {
                   password = val;
                 },
-              )
+              ),
+              SizedBox(height: 20,),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 17) ,
+                width: MediaQuery.of(context).size.width - 20,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.blue,
+                ),
+                alignment: Alignment.center,
+                child: Text("Sign In", style: TextStyle(color: Colors.white)),
+              ),
+              SizedBox(height: 20,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  Text(
+                    "Sign up",
+                      style: TextStyle(fontSize: 15,
+                        decoration: TextDecoration.underline,),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20,)
             ],
           ),
         ),
