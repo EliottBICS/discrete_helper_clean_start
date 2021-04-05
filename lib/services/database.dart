cc3a88389789184645a83991f11636445d60799f
EliottBICS <eliottbonte@gmail.com>
2021-04-05 10:55:38 +0200

    DatabaseService is now able to add data to an existing questionnaire

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,3 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class DatabaseService{
+
+  //function used to pass some data to the firestore database
+
+  Future<void> addQuestionnaireData(Map questionnaireData, String questionnaireId) async {
+    await FirebaseFirestore.instance.collection("Questionnaire")
+        .doc(questionnaireId).set(questionnaireData)
+        .catchError((e){print(e.toString());
+        });
+
+
 
 }
