e4cddd5e905f821c6e1058daded2cc11a886e6b1
EliottBICS <eliottbonte@gmail.com>
2021-04-05 12:44:44 +0200

    create_question.dart created - It is used to create a questionnaire by providing its title, description, and potentially an image to depict it (I'd like to make it optional later)
    will use a "add_question" widget that I still need to define

--- lib/views/create_question.dart
+++ lib/views/create_question.dart
@@ -1,5 +1,11 @@
+import 'package:discrete_helper_clean_start/services/database.dart';
+import 'package:discrete_helper_clean_start/views/add_question.dart';
 import 'package:discrete_helper_clean_start/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:random_string/random_string.dart';
+
+//used to create an online questionnaire
+
 
 class CreateQuestion extends StatefulWidget {
   @override
@@ -8,8 +14,44 @@ class CreateQuestion extends StatefulWidget {
 
 class _CreateQuestionState extends State<CreateQuestion> {
 
+  //Declare variables here
+
   final _formKey = GlobalKey<FormState>();
-  String  questionnaireTitle, questionnaireDescription, imgURL;
+  String  questionnaireTitle, questionnaireDescription, imgURL, questionnaireId;
+  DatabaseService databaseService = new DatabaseService();
+  // underscore makes a variable private (accessible only in this .dart)
+  bool _isLoading = false;
+
+  createQuestionnaire() async {
+    if(_formKey.currentState.validate()){
+
+      //setState is used to update a variable and refresh it at the same time
+      setState(() {
+        _isLoading = true;
+      });
+      
+      //generates the questionnaire ID randomly (16) chars
+      questionnaireId = randomAlphaNumeric(16);
+
+      //maps a key to a value, here QuestionnaireId to the randomly generated string
+      Map<String, String> questionnaireMap = {
+        "questionnaireId" : questionnaireId,
+        "imgUrl" : imgURL,
+        "questionnaireDescription" : questionnaireDescription,
+        "questionnaireTitle" : questionnaireTitle,
+      };
+
+      //sends this data to firestore with the function defined in database.dart
+      //then stop loading
+      await databaseService.addQuestionnaireData(questionnaireMap, questionnaireId).then((value){
+        setState(() {
+          _isLoading = false;
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => AddQuestion()));
+        });
+
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +60,10 @@ class _CreateQuestionState extends State<CreateQuestion> {
         title: discreteHelperAppBar(context),
         brightness: Brightness.dark,
       ),
-      body: Container(
+      //if we are still loading, show the first container, otherwise, show the second
+      body: _isLoading ? Container (
+        child: Center(child: CircularProgressIndicator()),
+      ) : Container(
         padding: EdgeInsets.symmetric(horizontal: 20),
         child: Form(
           key : _formKey,
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+//The homescreen. Users that are properly identified end up here
+//You can access questionnaires from here
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
