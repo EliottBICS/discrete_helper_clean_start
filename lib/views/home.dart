409b73fb5178a00a08177f1cfe09f71eec24d20d
EliottBICS <eliottbonte@gmail.com>
2021-04-25 08:25:22 +0200

    Listview before making the questionnaires clickable

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -33,7 +33,7 @@ class _HomeState extends State<Home> {
     //A list of available questionnaires in the form of a column
 
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 0.0,horizontal: 10.0),
+      margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
       child: Column(
         children: [
           StreamBuilder(
@@ -82,6 +82,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: BICSBlueAccent(), //For testing purposes
       appBar: AppBar(
         title: discreteHelperAppBar(context),
         brightness: Brightness.dark,
@@ -128,16 +129,35 @@ class Questionnaire extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
       //padding: EdgeInsets.symmetric(vertical: 10),
       height: 150,
       child: Stack(
         children: [
-          Image.network(imgUrl, width: MediaQuery.of(context).size.width - 20, fit: BoxFit.fitWidth,),
-          //the width is now dependant on the width of the screen used
-          //the image must ensure that its whole width is conserved, regardless of its heigth
+          ClipRRect(
+            child: Image.network(
+              imgUrl,
+              width: MediaQuery.of(context).size.width - 20,
+              fit: BoxFit.fitWidth,
+            ),
+            //the width is now dependant on the width of the screen used
+            //the image must ensure that its whole width is conserved, regardless of its heigth
+            borderRadius: BorderRadius.circular(15),
+            //I make the edges of the rectangles round, with a radius of 15
+          ),
           Container(
+            
+            alignment: Alignment.center,
             child: Column(
-              children: [Text(title), Text(description)],
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FittedBox(
+                    child: Text(
+                  title,
+                  style: TextStyle(color: BICSWhite(), fontWeight: FontWeight.bold, fontSize: 20),
+                )),
+                FittedBox(child: Text(description, style: TextStyle(color: BICSWhite())),)
+              ],
             ),
           )
         ],
